b07fd2e8a58d33fa80e6b8af1dfdea9875f6a1be
Hans Goor <rooteddev@outlook.com>
2019-11-22 13:35:38 +0100

    Open Pretzel in new window.

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 /usr/bin/obs &
-/usr/bin/google-chrome-stable https://app.pretzel.rocks/player &
+/usr/bin/google-chrome-stable --new-window https://app.pretzel.rocks/player &
 /home/eyedevelop/Documents/OBS/screen_daemon.py
