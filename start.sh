1c366435e1f7af2e5baa25d2bf11778f8caea8d7
Hans Goor <rooteddev@outlook.com>
2019-11-22 13:38:05 +0100

    Made user universal.

--- start.sh
+++ start.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# Check if being ran from the right directory.
+if [[ ! -f "screen_daemon.py" ]]; then
+    echo "ERROR: Please run the script in the main directory."
+    exit 1
+fi
+
 /usr/bin/obs &
 /usr/bin/google-chrome-stable --new-window https://app.pretzel.rocks/player &
-/home/eyedevelop/Documents/OBS/screen_daemon.py
+./screen_daemon.py
