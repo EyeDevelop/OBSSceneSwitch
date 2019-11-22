813b4051dcedbfe1b85b64e4e20c7d6be394aa67
Hans Goor <rooteddev@outlook.com>
2019-11-22 13:48:21 +0100

    Added a Desktop Entry creator.

---
+++ gen_launcher.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Check if in the right directory.
+if [[ ! -f "screen_daemon.py" ]]; then
+    echo "ERROR: Run this script in the repo directory."
+    exit 1
+fi
+
+if [[ ! -d "$HOME/.local/share/applications" ]]; then
+    echo "ERROR: Cannot find user applications folder."
+    echo "       Please create ~/.local/share/applications"
+    exit 1
+fi
+
+# Create a new launcher.
+cat << EOF > $HOME/.local/share/applications/stream-setup.desktop
+[Desktop Entry]
+Categories=Development;
+Comment=Starts OBS and the rest of the stream setup.
+Path="$(pwd)"
+Exec="start.sh"
+Icon=/usr/share/icons/hicolor/256x256/apps/com.obsproject.Studio.png
+Name=Start Stream Setup
+Terminal=true
+Type=Application
+Version=1.0
+EOF
+
+
+echo "Successfully created desktop entry!"
